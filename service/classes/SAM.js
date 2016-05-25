849078d1a588a2f910b12f567bdbf7aaa067bdc2
Jaap <jh.vermaire@bbn.nl>
2016-05-25 22:51:27 +0200

    refactor ES6 class

--- service/classes/SAM.js
+++ service/classes/SAM.js
@@ -6,54 +6,56 @@ const uuid = require('node-uuid');
 
 const fs = require('fs');
 const path_module = require('path');
-var actions = [];
-var plugins = [];
-var plugin;
-var pluginInstance;
-
-function LoadPlugins(path) {
-  console.log('Loading modules in path:', path);
-  fs.lstat(path, function(err, stat) {
-    console.log('stat.isDirectory()', stat.isDirectory());
-    if (stat.isDirectory()) {
-      // we have a directory: do a tree walk
-      fs.readdir(path, function(err, files) {
-        var f, l = files.length;
-        for (var i = 0; i < l; i++) {
-          console.log('loading:', files[i]);
-          f = path_module.join(path, files[i]);
-          LoadPlugins(f);
-        }
-      });
-    } else {
-      // we have a file: load it
-      plugin = require(path);
-      pluginInstance = new plugin();
-      plugins.push(pluginInstance);
-      actions.push(pluginInstance.getActions());
-
-
-    }
-  });
-}
-var DIR = path_module.join(__dirname, '../plugins');
-LoadPlugins(DIR);
-
 
 class SAM extends EventEmitter {
 
   constructor() {
+
     super();
-    this.on('speech in', this.process.bind(this));
-    this.wit = new Wit('E6AWMDVXHBRMM2TOIX3M53GNREPBFS7A', {
+    
+    var DIR = path_module.join(__dirname, '../plugins');
+
+    this.actions =
+    {
       say: this.say.bind(this),
       merge: this.merge.bind(this),
       error: this.error.bind(this)
-    });
+    };
+    
+    this.plugins = [];
+    this.LoadPlugins(DIR);
+    this.on('speech in', this.process.bind(this));
+    this.wit = new Wit('E6AWMDVXHBRMM2TOIX3M53GNREPBFS7A', this.actions);
     this.sessionId = uuid.v1();
     this.context = {};
     this.steps = 5;
   }
+
+
+  LoadPlugins(path) {
+    console.log('Loading modules in path:', path);
+    fs.lstat(path, (err, stat) => {
+      console.log('stat.isDirectory()', stat.isDirectory());
+      if (stat.isDirectory()) {
+        // we have a directory: do a tree walk
+        fs.readdir(path, (err, files) => {
+          var f, l = files.length;
+          for (var i = 0; i < l; i++) {
+            console.log('loading:', files[i]);
+            f = path_module.join(path, files[i]);
+            this.LoadPlugins(f);
+          }
+        });
+      } else {
+        // we have a file: load it
+        var plugin = require(path);
+        var pluginInstance = new plugin();
+        this.plugins.push(pluginInstance);
+        // this.actions.push(pluginInstance.getActions());
+      }
+    });
+  }
+
   
   say(sessionId, context, message, cb) {
     console.log('sam say:' , message, context);
