a91f76c5a9a67168acade1f66ffc7414110beabb
Jaap <jh.vermaire@bbn.nl>
2016-05-25 12:07:58 +0200

    TE weinig cat memes :?
    Static route werkt.

--- service/index.js
+++ service/index.js
@@ -1,6 +1,10 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+
+app.use(express.static('../app'));
 
 io.on('connection', (socket) => {
   console.log('New connection');
