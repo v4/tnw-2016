e2ba882ec4ac963f91073b1cc9ec5a3bfbf30342
Jaimy de Graaf <v4@serapine.nl>
2016-05-25 18:58:47 +0200

    base plugin

---
+++ service/classes/Plugin.js
@@ -0,0 +1,13 @@
+'use strict';
+  
+const EventEmitter = require('events').EventEmitter;
+
+class Plugin extends EventEmitter {
+
+  constructor() {
+    super();
+  }
+ 
+}
+
+module.exports = Plugin;
