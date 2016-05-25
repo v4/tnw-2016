e93f47a165a03f92da79f904c496a82210e29af3
Michael <maggelo@gmail.com>
2016-05-25 16:34:05 +0200

    renamed face.js to tapeface.js, which it is.

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,19 @@
-var socket = io('http://localhost:3000');
+var socket = io('http://localhost:3000')
 socket.on('speech out', function (data) {
-  console.log(data);
-});
+  console.log(data)
+})
 
-var recognition = new webkitSpeechRecognition();
-recognition.continuous = true;
-recognition.interimResults = true;
-recognition.onresult = function(event) {
-  console.log(event);
-  if(event.results[0].isFinal){
-    console.log('SOCKETING:', event.results[0][0].transcript);
-    socket.emit('speech in', { payload: event.results[0][0].transcript });
+var recognition = new webkitSpeechRecognition()
+recognition.continuous = true
+recognition.interimResults = true
+recognition.onresult = function (event) {
+  console.log(event)
+  if (event.results[0].isFinal) {
+    console.log('SOCKETING:', event.results[0][0].transcript)
+    socket.emit('speech in', { payload: event.results[0][0].transcript })
   }
 }
-recognition.start();
+recognition.start()
+
+// face API testing 1,2,3
+face.test()
