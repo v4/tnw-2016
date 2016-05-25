94e9cd731eaed062af3973f428bd32a3139a9f77
Jaap <jh.vermaire@bbn.nl>
2016-05-25 14:44:46 +0200

    socketing wip

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,12 @@
+var socket = io('http://localhost:3000');
+socket.on('speech out', function (data) {
+  console.log(data);
+});
+
 var recognition = new webkitSpeechRecognition();
 recognition.continuous = true;
 recognition.interimResults = true;
 recognition.onresult = function(event) {
-  console.log(event)
+  socket.emit('speech in', { paylouad: event });
 }
 recognition.start();
