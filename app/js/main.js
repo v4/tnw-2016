4ce3d0be0edfde9bfacb5537d830f3950631a16a
Jaimy de Graaf <v4@serapine.nl>
2016-05-25 23:34:35 +0200

    Fixed mouth

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,67 @@
-var recognition = new webkitSpeechRecognition();
-var socket = io('http://localhost:3000');
-socket.on('speech out', (data) => {
-  console.log('speech out', data.payload);
-  var audio = new Audio('data:audio/ogg;base64,' + data.payload);
-  audio.play();
-  recognition.start();
-});
+function dataURItoBlob(dataURI)
+  {
+      // Split the input to get the mime-type and the data itself
+      dataURI = dataURI.split( ',' );
 
+      // First part contains data:audio/ogg;base64 from which we only need audio/ogg
+      var type = dataURI[ 0 ].split( ':' )[ 1 ].split( ';' )[ 0 ];
 
-recognition.continuous = true;
-recognition.interimResults = true;
-recognition.onresult = function(event) {
-  face.makeHappy();
-  console.log(event);
-  if(event.results[0].isFinal) {
-    console.log('speech in', event.results[0][0].transcript);
-    socket.emit('speech in', { payload: event.results[0][0].transcript });
-    recognition.stop();
+      // Second part is the data itself and we decode it
+      var byteString = atob( dataURI[ 1 ] );
+      var byteStringLen = byteString.length;
+
+      // Create ArrayBuffer with the byte string and set the length to it
+      var ab = new ArrayBuffer( byteStringLen );
+
+      // Create a typed array out of the array buffer representing each character from as a 8-bit unsigned integer
+      var intArray = new Uint8Array( ab );
+      for ( var i = 0; i < byteStringLen; i++ ) 
+      {
+          intArray[ i ] = byteString.charCodeAt( i );
+      }
+
+      return new Blob( [ intArray ], {type: type} );
   }
-};
-
-socket.on('speech out', (message) =>{
-  console.log('speech out', message)
-});
-
+  
 $(document).ready(function () {
+
+  var recognition = new webkitSpeechRecognition();
+  var socket = io('http://localhost:3000');
+  var audio = null;
+  socket.on('speech out', (data) => {
+    if (audio) audio = null;
+    console.log('speech out', data.payload);
+    audio = new Audio();
+    audio.id = 'audioElement';
+    audio.src = URL.createObjectURL(dataURItoBlob('data:audio/ogg;base64,' + data.payload));
+    audio.load();
+    audio.play();
+    mouth.create(audio, '.face-oscilloscope', {
+      frequencyBins : 20,
+      barPadding: 2,
+      height: '50',
+      width: '75',
+      fillColor: '#3541db'
+    });
+    recognition.start();
+  });
+
+
+  recognition.continuous = true;
+  recognition.interimResults = true;
+  recognition.onresult = function(event) {
+    face.makeHappy();
+    console.log(event);
+    if(event.results[0].isFinal) {
+      console.log('speech in', event.results[0][0].transcript);
+      socket.emit('speech in', { payload: event.results[0][0].transcript });
+      recognition.stop();
+    }
+  };
+
+  socket.on('speech out', (message) =>{
+    console.log('speech out', message)
+  });
   
   recognition.start();
 
@@ -39,14 +76,4 @@ $(document).ready(function () {
   // face.setVocalExpression('i');
   // face.setVocalExpression('');
   
-  mouth.create('audioElement', '.face-oscilloscope', {
-    frequencyBins : 20,
-    barPadding: 2,
-    height: '50',
-    width: '75',
-    fillColor: '#3541db'
-  });
-
-  mouth.start();
-
 });
