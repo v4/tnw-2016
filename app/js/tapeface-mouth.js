e50ef17d351acc4cb76cc45adb60d059e6c8badc
Michael <maggelo@gmail.com>
2016-05-25 21:13:46 +0200

    removed oscilloscope, added in D3 spectrum analyzer.

--- app/js/tapeface-mouth.js
+++ app/js/tapeface-mouth.js
@@ -4,38 +4,75 @@ var mouth = (function () { // eslint-disable-line
   
   var module = {}; // eslint-disable-line
   
-  module.oscillator = null;
-  module.oscilloscope = null;
-
   module.create = function (container) {
-    // Create an audio-context
-    var audioContext = new window.AudioContext();
-    this.oscillator = audioContext.createOscillator();
-    this.oscillator.type = 'sine';
-    this.oscillator.frequency.value = 400;
-    this.oscillator.start();
-
-    // Create an Oscilloscope instance
-    //   Parameters:
-    //     - The container in which the oschilloscope gets created
-    //     - an optional audio-context on which the oscilloscope creates an analyser-node,
-    //          and can connect to the destination.
-    //          If no audio-context is specified, a new one will be created created.
-    
-    this.oscilloscope = new Oscilloscope(container, audioContext);
-    
-    // Connect the oscillator-node to the oscilloscope
-    this.oscillator.connect(this.oscilloscope.analyserNode);
   }
 
   module.get = function () {
-    return this.oscilloscope;
   }
 
   module.start = function () {
-    // Start the oscilloscope
-    this.oscilloscope.start();
   }
 
   return module;
 }());
+
+
+$(document).ready(function () {
+
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  var audioElement = document.getElementById('audioElement');
+  var audioSrc = audioCtx.createMediaElementSource(audioElement);
+  var analyser = audioCtx.createAnalyser();
+
+  // Bind our analyser to the media element source.
+  audioSrc.connect(analyser);
+  audioSrc.connect(audioCtx.destination);
+
+  //var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+  var frequencyData = new Uint8Array(200);
+
+  var svgHeight = '200';
+  var svgWidth = '500';
+  var barPadding = '1';
+
+  function createSvg(parent, height, width) {
+    return d3.select(parent).append('svg').attr('height', height).attr('width', width);
+  }
+
+  var svg = createSvg('body', svgHeight, svgWidth);
+
+  // Create our initial D3 chart.
+  svg.selectAll('rect')
+     .data(frequencyData)
+     .enter()
+     .append('rect')
+     .attr('x', function (d, i) {
+        return i * (svgWidth / frequencyData.length);
+     })
+     .attr('width', svgWidth / frequencyData.length - barPadding);
+
+  // Continuously loop and update chart with frequency data.
+  function renderChart() {
+     requestAnimationFrame(renderChart);
+
+     // Copy frequency data to frequencyData array.
+     analyser.getByteFrequencyData(frequencyData);
+
+     // Update d3 chart with new data.
+     svg.selectAll('rect')
+        .data(frequencyData)
+        .attr('y', function(d) {
+           return svgHeight - d;
+        })
+        .attr('height', function(d) {
+           return d;
+        })
+        .attr('fill', function(d) {
+           return 'rgb(0, 0, ' + d + ')';
+        });
+  }
+
+  // Run the loop
+  renderChart();
+
+});
