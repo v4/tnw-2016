f419969639df97783898e688a50fa8c1d1be8372
Milan Saleh <mt.saleh@bbn.nl>
2016-05-25 17:25:00 +0200

    set tapefases + stories

--- app/js/tapeface.js
+++ app/js/tapeface.js
@@ -7,7 +7,7 @@
 console.log('tapeface.js being loaded...')
 
 var face = (function () { // eslint-disable-line
-  var module = {} // eslint-disable-line
+  var module = {}; // eslint-disable-line
   // var privateVariable = 1 // eslint-disable-line
 
   // function privateMethod () { // eslint-disable-line
@@ -18,24 +18,39 @@ var face = (function () { // eslint-disable-line
 
   module.makeAngry = function () {
     console.log('tapeface is angry.')
-  }
+    this.setBrowseAndEyesMood('angry');
+  };
 
   module.makeSad = function () {
     console.log('tapeface is sad.')
-  }
+    this.setBrowseAndEyesMood('sad');
+  };
 
   module.makeHappy = function () {
     console.log('tapeface is happy.')
-  }
+    this.setBrowseAndEyesMood('happy');
+  };
 
   module.makeNeutral = function () {
     console.log('tapeface is neutral.')
-  }
+    this.setBrowseAndEyesMood('neutral');
+  };
 
   module.setVocalExpression = function (expression) {
     expression = expression || 'silent';
     console.log('tapeface vocal expression set to: ', expression);
+  };
+
+  module.removeAllBrowseAndEyesClasses = function () {
+    $('.face-brows').removeClass();
+    $('.face-eyes').removeClass();
   }
 
-  return module
-}())
+  module.setBrowseAndEyesMood = function (mood) {
+    this.removeAllBrowseAndEyesClasses();
+    $('.face-eyes').addClass('.face-' . mood);
+    $('.face-brows').addClass('.face-' . mood);
+  }
+
+  return module;
+}());
